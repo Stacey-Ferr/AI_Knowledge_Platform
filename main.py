cb2b22bc2504590c095bd8aa7b00ba3d5195240a
Stacey-Ferr <staceyferreira10@gmail.com>
2026-06-02 17:49:34 +0530

    Adding middleware for request logging

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from api.routes.health import router as health_router
+from middleware.request_logger import RequestLoggingMiddleware
 
 app = FastAPI()
 
 app.include_router(health_router)
+
+app.add_middleware(RequestLoggingMiddleware)
